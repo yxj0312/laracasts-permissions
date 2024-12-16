506196a2f4a37b2d5479db9bea3e9ddc051be23a
yxj0312 <yanxiajun@hotmail.com>
2024-12-16 23:17:56 +0100

    change migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin'
     ];
 
     /**
@@ -48,6 +49,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_admin' => 'boolean'
     ];
 
     /**
